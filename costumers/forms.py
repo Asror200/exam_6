fb562996fa8695910ab1e14ed5d978c2d69020f7
Asror <asrorberdiyev1202@gmail.com>
2024-08-22 19:32:10 +0500

    from email was done optional

--- costumers/forms.py
+++ costumers/forms.py
@@ -9,7 +9,13 @@ class AddCustomerForm(forms.ModelForm):
 
 
 class SendEmailForm(forms.Form):
-    from_email = forms.EmailField()
-    to_email = forms.EmailField()
+    from_email = forms.EmailField(required=False, label='optional')
+    to_email = forms.EmailField(label='must')
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
+
+    def clean_from_email(self):
+        from_email = self.cleaned_data.get('from_email')
+        if not from_email:
+            return 'berdiyevasror12@gmail.com'
+        return from_email
